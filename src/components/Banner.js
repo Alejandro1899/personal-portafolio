4ff6f64e977e182211705f062a1b73bcea5bee89
Luis Joel Cristino Arteaga <ale.arteaga99@outlook.com>
2023-05-07 01:50:47 -0600

    Correc

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,49 +3,48 @@ import { Col, Container, Row } from "react-bootstrap";
 import {ArrowRightCircle} from 'react-bootstrap-icons';
 import headerImg from '../assets/img/header-img.svg';
 
-export const Banner = ()=>{
-    const [loopNum, setLoopNum,]=useState(0);
-    const[isDeleting, setIsDeleting]= useState(false);
-    const[text,setTex]=useEffect('');
-    const[index, setIndex]= useState(1);
-    const toRotate =["web Developer","Web Designer","UX/UI Designer"];
-    const [delta,setDelta]= useState(300- Math.random()*100)
-    const period =2000;
-
-    useEffect(()=>{
-        let ticker = setInterval(()=>{
-            tick();
-        }, delta)
-        return ()=>{clearInterval(ticker)};
-    },[text])
-
-    const tick= ()=> {
-        let i= loopNum % toRotate.length;
-        let fullText = toRotate[i];
-        let updateText = isDeleting ? fullText.substring (0,text.length - 1): fullText.substring(0,text.length +1);
-
-        setTex(updateText);
-
-        if  (isDeleting){
-            setDelta(prevDelta=> prevDelta /2)
-        }
-
-        if (!isDeleting && updateText === fullText){
-            setIsDeleting(true);
-            setIndex(prevIndex=> prevIndex-1);
-            setDelta(period);
-            
-        }  else if( isDeleting && updateText === ''){
-            setIsDeleting(false);
-            setLoopNum (loopNum + 1);
-            setIndex(1);
-            setDelta(500);
-        }else{
-            setIndex(prevIndex => prevIndex +1);
-        }
+export const Banner = () => {
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const [index, setIndex] = useState(1);
+    const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
+    const period = 2000;
+  
+    useEffect(() => {
+      let ticker = setInterval(() => {
+        tick();
+      }, delta);
+  
+      return () => { clearInterval(ticker) };
+    }, [text])
+  
+    const tick = () => {
+      let i = loopNum % toRotate.length;
+      let fullText = toRotate[i];
+      let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+  
+      setText(updatedText);
+  
+      if (isDeleting) {
+        setDelta(prevDelta => prevDelta / 2);
+      }
+  
+      if (!isDeleting && updatedText === fullText) {
+        setIsDeleting(true);
+        setIndex(prevIndex => prevIndex - 1);
+        setDelta(period);
+      } else if (isDeleting && updatedText === '') {
+        setIsDeleting(false);
+        setLoopNum(loopNum + 1);
+        setIndex(1);
+        setDelta(500);
+      } else {
+        setIndex(prevIndex => prevIndex + 1);
+      }
     }
-
-    
+  
 
     return (
       <section className="banner" id="home">
